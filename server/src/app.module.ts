a0875ed34a664f29e413ad1a4ef442d3d6fd7bb3
IgorChab <chabanchukigor@gmail.com>
2023-03-02 17:36:07 +0300

    commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { ConfigModule } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import {AuthModule} from "./auth/auth.module";
 import {FileModule} from "./files/file.module";
 import {ServeStaticModule} from "@nestjs/serve-static";
@@ -8,7 +8,7 @@ import * as path from 'path'
 
 @Module({
   imports: [
-    //   ConfigModule.forRoot(),
+      ConfigModule.forRoot(),
       MongooseModule.forRoot('mongodb://127.0.0.1:27017/cloud-drive'),
       ServeStaticModule.forRoot({
           rootPath: path.join(__dirname, '..', 'storage'),
