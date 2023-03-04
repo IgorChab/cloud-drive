c840de4cc06173e8846abf9444bb88aea511712d
IgorChab <chabanchukigor@gmail.com>
2023-03-04 21:08:15 +0300

    commit

--- client/src/components/PinnedFolder/PinnedFolder.tsx
+++ client/src/components/PinnedFolder/PinnedFolder.tsx
@@ -23,7 +23,7 @@ const PinnedFolder: FC<Props> = ({id, size, items, name, preview}) => {
 
     return (
         <div 
-            className={`border border-[#F5F5F5] p-[12px_30px] gap-6 flex items-center cursor-pointer ${preview? 'justify-center' : 'justify-between'}`}
+            className={`border border-[#F5F5F5] p-[12px_30px] gap-6 flex items-center cursor-pointer ${preview? 'justify-center cursor-default' : 'justify-between'}`}
             title={name}
         >
             {
