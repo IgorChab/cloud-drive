2902c7305b737086ecfec9e83578aa3fb7777e7e
IgorChab <chabanchukigor@gmail.com>
2023-03-07 17:40:48 +0300

    commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -11,7 +11,9 @@ import * as path from 'path'
       ConfigModule.forRoot(),
       MongooseModule.forRoot('mongodb://127.0.0.1:27017/cloud-drive'),
       ServeStaticModule.forRoot({
-          rootPath: path.join(__dirname, '..', '..', 'client', 'build'),
+        serveRoot: '/build',
+        rootPath: path.join(__dirname, '..', '..', 'client', 'build'),
+        exclude: ['api/*']
       }),
       AuthModule,
       FileModule
