797d7b6691a8ad7322c3e18a6b45874ea81094b6
IgorChab <chabanchukigor@gmail.com>
2023-03-09 16:11:27 +0300

    commit

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,14 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser())
   app.enableCors({
     credentials: true,
-    origin: 'https://chatbook.space'
+    origin: ['https://chatbook.space', 'https://www.chatbook.space']
   })
   app.setGlobalPrefix('api')
   await app.listen(5000);
